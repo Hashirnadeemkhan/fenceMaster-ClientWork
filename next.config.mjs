68939da9cef7e84635b763fb3d6ad6aab2c601e5
Hashir Nadeem Khan <khashir657@gmail.com>
2025-09-16 12:24:18 +0500

    updation

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
-// next.config.js or next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true, // Disable image optimization for static export
-    },
-  };
-  
-  export default nextConfig; // Use this for next.config.mjs (ES Module)
-  // module.exports = nextConfig; // Use this for next.config.js (CommonJS)
-  
+  reactStrictMode: true,
+  images: {
+    unoptimized: false // default rakho, Resend aur forms se related nahi hai
+  }
+};
+
+export default nextConfig;
